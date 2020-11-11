b6a3c7e0513da723535dda75d08ed922ad390c0f
revvin76 <32345166+revvin76@users.noreply.github.com>
2020-11-11 09:55:59 +0000

    Compliance update
    Moved menu registration into the initialise function.
    Removed "ossn" from function names and references.

--- ossn_com.php
+++ ossn_com.php
@@ -10,14 +10,23 @@
  */
 //setting up path so we can use it in entire file 
 //if your component folder have upper and lower case characters please use same here.
-define('__OSSN_WEB_CHAT__', ossn_route()->com . 'WebChat/');
+define('__WEB_CHAT__', ossn_route()->com . 'WebChat/');
 
-//this function is used to initilize ossn
-function ossn_web_chat() {
+//this function is used to initilize webchat
+function web_chat() {
    ossn_register_page('webchat', 'webchat_template_page');
    ossn_register_page('chat_api', 'chat_api');
+   if(ossn_isLoggedin()) {
+		$icon          = ossn_site_url('components/OssnMessages/images/messages.png');
+		ossn_register_sections_menu('newsfeed', array(
+				'name' => 'webchat',
+				'text' => ossn_print('com:webchat:menu'),
+				'url' => ossn_site_url('webchat'),
+				'parent' => 'links',
+				'icon' => $icon
+		));
+	}
 }
-
 function webchat_template_page(){
     	$content = ossn_plugin_view('webchat/webchat_page');
 		$title = 'Chat';
@@ -28,17 +37,6 @@ function chat_api(){
 		$title = 'Chat_API';
     	echo ossn_view_page($title, $content, 'chat_api_template');	
 }
-ossn_register_callback('ossn', 'init', 'ossn_web_chat');
 
-if(ossn_isLoggedin()) {
+ossn_register_callback('ossn', 'init', 'web_chat');
 
-		$icon          = ossn_site_url('components/OssnMessages/images/messages.png');
-		ossn_register_sections_menu('newsfeed', array(
-				'name' => 'webchat',
-				'text' => ossn_print('com:webchat:menu'),
-				'url' => ossn_site_url('webchat'),
-				'parent' => 'links',
-				'icon' => $icon
-		));
-		
-}
