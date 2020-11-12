3ae7b1c3f76f519d08c2ff1dc211095656b135d2
revvin76 <revvin76@gmail.com>
2020-11-12 22:06:48 +0000

    Check for logon removed from CHAT_API registration as it breaks functionality. Remains on webchat page registration - forcing a 404 error.

--- ossn_com.php
+++ ossn_com.php
@@ -14,14 +14,16 @@ define('__WEB_CHAT__', ossn_route()->com . 'WebChat/');
 
 //this section initialises the API extensions
 function unread_messages_count_api() {
-		ossn_add_hook('services', 'methods', 'unread_mesages_count_api_custom');
+	ossn_add_hook('services', 'methods', 'unread_mesages_count_api_custom');
 }
 function unread_mesages_count_api_custom($hook, $type, $methods, $params) {
 		$methods['v1.0'][] = 'unread_mesages_count_custom';
 		return $methods;
 }
+
 //this section initilises webchat
 function web_chat() {
+    if(ossn_isLoggedin()) {
 		ossn_register_page('webchat', 'webchat_template_page');
 		ossn_register_page('chat_api', 'chat_api');
 		$icon          = ossn_site_url('components/OssnMessages/images/messages.png');
@@ -32,6 +34,7 @@ function web_chat() {
 				'parent' => 'links',
 				'icon' => $icon
 		));
+	}
 }
 function webchat_template_page(){
     	$content = ossn_plugin_view('webchat/webchat_page');
@@ -44,13 +47,6 @@ function chat_api(){
     	echo ossn_view_page($title, $content, 'chat_api_template');	
 }
 
-if(ossn_isLoggedin()) {
-	ossn_register_callback('ossn', 'init', 'unread_messages_count_api');
-	ossn_register_callback('ossn', 'init', 'web_chat');
-} else {
-	
-	if (strtolower($_SERVER['REQUEST_URI']) == "/webchat") {
-		echo ("<script>alert ('-".$_SERVER['REQUEST_URI']."-');</script>");
-		header("Location: /login");
-	}
-}
+ossn_register_callback('ossn', 'init', 'unread_messages_count_api');
+ossn_register_callback('ossn', 'init', 'web_chat');
+
