62956bc919499ae47b9b7f17f3fc8dbb4d1b1a97
revvin76 <revvin76@gmail.com>
2020-11-15 11:24:36 +0000

    Added PWA options - still WIA as unable to force A2HS prompt?

--- plugins/default/theme/page/webchat_page_template.php
+++ plugins/default/theme/page/webchat_page_template.php
@@ -27,6 +27,8 @@ if (isset($params['contents'])) {
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title><?php echo $title; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="manifest" href="<?php echo ossn_site_url('components/WebChat/manifest.json');?>">
+	<meta name="theme-color" content="#3367D6">
     <link rel="icon" href="<?php echo ossn_add_cache_to_url(ossn_theme_url('images/favicon.ico'));?>" type="image/x-icon" />
 	<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css'>
 	<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.2/css/font-awesome.min.css'>
@@ -37,6 +39,39 @@ if (isset($params['contents'])) {
 </head>
 
 <body>
+ <button class="add-button">Add to home screen</button>
  <?php echo $contents; ?>
+<script>
+// Code to handle install prompt on desktop
+
+let deferredPrompt;
+const addBtn = document.querySelector('.add-button');
+addBtn.style.display = 'none';
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI to notify the user they can add to home screen
+  addBtn.style.display = 'block';
+
+  addBtn.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    addBtn.style.display = 'none';
+    // Show the prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+  });
+});
+</script>
 </body>
 </html>
