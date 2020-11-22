e04e2eca2cac22a6cdb35dd2730dfda6470fb2bc
revvin76 <revvin76@gmail.com>
2020-11-22 13:50:00 +0000

    Tidied up page registration and introduced array extension for Private Network.

--- ossn_com.php
+++ ossn_com.php
@@ -18,21 +18,28 @@ function unread_mesages_count_api_custom($hook, $type, $methods, $params) {
 		return $methods;
 }
 
+function com_webchat_extend_private_network($hook, $type, $allowed_pages, $params) {
+    $allowed_pages[0][] = 'chat_api';    // pages to be addressed by 1 path element
+    return $allowed_pages;
+}
+
 //this section initilises webchat
 function web_chat() {
 	ossn_add_hook('services', 'methods', 'unread_mesages_count_api_custom');
 	ossn_extend_view('css/ossn.default', 'css/main');
-
+	ossn_add_hook('private:network', 'allowed:pages', 'com_webchat_extend_private_network');
+	ossn_register_page('webchat', 'webchat_template_page');
+		
     if(ossn_isLoggedin()) {
 		$component = new OssnComponents;
 		$settings = $component->getComSettings('webchat');
 				
+		// Register the admin dashboard for administrators		
 		if(ossn_isAdminLoggedin()) {
 			ossn_register_action('webchat/admin/settings', __WEBCHATADMIN__ . 'actions/webchat/admin/settings.php');
 			ossn_register_com_panel('webchat', 'settings');
-		}
-		
-		ossn_register_page('webchat', 'webchat_template_page');
+		}		
+
 		ossn_register_page('chat_api', 'chat_api');
 		$icon          = ossn_site_url('components/OssnMessages/images/messages.png');
 		ossn_register_sections_menu('newsfeed', array(
@@ -43,20 +50,19 @@ function web_chat() {
 				'icon' => $icon
 		));
 	} else {
-		ossn_register_page('webchat', 'webchat_redirect');
+
 	}
 }
 function webchat_template_page(){
+	if(ossn_isAdminLoggedin()) {
     	$content = ossn_plugin_view('webchat/webchat_page');
 		$title = 'Chat';
     	echo ossn_view_page($title, $content, 'webchat_page_template');	
+	}
 }
 function chat_api(){
     	$content = ossn_plugin_view('webchat/chat_api');
 		echo $content;	
-}
-function webchat_redirect(){
-	redirect ("login");
 }
 
 ossn_register_callback('ossn', 'init', 'web_chat');
